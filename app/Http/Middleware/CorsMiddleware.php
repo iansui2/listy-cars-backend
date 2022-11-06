bf2a774f1e306226e1bf57ac57956a2bc3e2866c
iansui2 <iansui200@gmail.com>
2022-11-06 19:25:51 +0800

    updated cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -19,7 +19,7 @@ class CorsMiddleware
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, securitytoken, x-jwt-token'
         ];
 
         if ($request->isMethod('OPTIONS'))
@@ -27,13 +27,12 @@ class CorsMiddleware
             return response()->json('{"method":"OPTIONS"}', 200, $headers);
         }
 
-        $response = $next($request);
-        foreach($headers as $key => $value)
-        {
-            $response->header($key, $value);
-        }
-
-        return $response;
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Max-Age', '86400')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, securitytoken, x-jwt-token'); 
     }
 }
 ?>
